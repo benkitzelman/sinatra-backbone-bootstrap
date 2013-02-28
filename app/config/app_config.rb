bd40d56034641a46f9de2472a5e94b39dbbbb82b
Ben Kitzelman <benkitzelman@gmail.com>
2013-02-28 20:06:51 +1100

    cleanup

--- app/config/app_config.rb
+++ app/config/app_config.rb
@@ -1,11 +1,2 @@
 # common config
 puts "-- loading #{Bootstrap.environment} environment"
-# require 'settingslogic'
-# module GreyNomad
-#   module Config
-#     class AppConfig < ::Settingslogic
-#       source  File.join File.dirname(__FILE__), "app_config.yml"
-#       namespace GreyNomad.environment.to_s
-#     end
-#   end
-# end
--- assets/javascripts/app/views/view.coffee
+++ assets/javascripts/app/views/view.coffee
@@ -1,10 +1,11 @@
 window.App ?= {}
 class App.View extends Backbone.View
   className: 'simple-view'
+
   template: _.template '''
-  <img src='/images/fonzie.jpg'>
-  <h1>Good to go...</h1>
-  <p>Happy coding</p>
+    <img src='/images/fonzie.jpg'>
+    <h1>Good to go...</h1>
+    <p>Happy coding</p>
   '''
 
   render: ->
